a213e085a72fe3d2f52c5c3938b7a62c414bbc03
Akarshit <akarshitwal@gmail.com>
2017-06-30 11:24:16 +0530

    Try catch

--- controller/product.js
+++ controller/product.js
@@ -9,19 +9,23 @@ const Product = require(path.join(global.SERVER_ROOT, 'mongo-model', 'product'))
 const { mapAsync } = require(path.join(global.SERVER_ROOT, 'utils'));
 
 api.getProduct = async (req, res) => {
-    const id = Number(req.params.id);
-    if (is.not.number(id)) {
-        res.status(400).json("Invalid Id");
-        return;
+    try {
+        const id = Number(req.params.id);
+        if (is.not.number(id)) {
+            res.status(400).json("Invalid Id");
+            return;
+        }
+        const product = await Product.getProduct(id);
+        if (product === null) {
+            res.status(400).json("Id not in database");
+            return;
+        }
+        const metadata = JSON.parse(await rp(`https://api.target.com/products/v3/${id}?fields=descriptions&id_type=TCIN&key=43cJWpLjH8Z8oR18KdrZDBKAgLLQKJjz`));
+        product.name = sa(metadata, 'product_composite_response.items[0].online_description.value')
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json(err);
     }
-    const product = await Product.getProduct(id);
-    if (product === null) {
-        res.status(400).json("Id not in database");
-        return;
-    }
-    const metadata = JSON.parse(await rp(`https://api.target.com/products/v3/${id}?fields=descriptions&id_type=TCIN&key=43cJWpLjH8Z8oR18KdrZDBKAgLLQKJjz`));
-    product.name = sa(metadata, 'product_composite_response.items[0].online_description.value')
-    res.status(200).json(product);
 }
 
 api.putProduct = async (req, res) => {
@@ -31,25 +35,29 @@ api.putProduct = async (req, res) => {
 }
 
 api.updatePrice = async (req, res) => {
-    const product = req.body;
-    if (is.not.propertyDefined(product, 'current_price')
-        || is.not.propertyDefined(product.current_price, 'value')
-        || is.not.propertyDefined(product.current_price, 'currency_code')
-        || is.not.number(product.current_price.value)) {
-        res.status(400).json({ message: "body is not in expected format", format: { current_price: { value: "Integer", currency_code: "String"}}});
-        return;
+    try {
+        const product = req.body;
+        if (is.not.propertyDefined(product, 'current_price')
+            || is.not.propertyDefined(product.current_price, 'value')
+            || is.not.propertyDefined(product.current_price, 'currency_code')
+            || is.not.number(product.current_price.value)) {
+            res.status(400).json({ message: "body is not in expected format", format: { current_price: { value: "Integer", currency_code: "String"}}});
+            return;
+        }
+        const id = Number(req.params.id);
+        if (is.not.number(id)) {
+            res.status(400).json("Invalid Id");
+            return;
+        }
+        const result = await Product.updatePrice(id, product);
+        if (result === null) {
+            res.status(400).json("Id not in database");
+            return;
+        }
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(500).json(err);
     }
-    const id = Number(req.params.id);
-    if (is.not.number(id)) {
-        res.status(400).json("Invalid Id");
-        return;
-    }
-    const result = await Product.updatePrice(id, product);
-    if (result === null) {
-        res.status(400).json("Id not in database");
-        return;
-    }
-    res.status(200).json(result);
 }
 
 module.exports = mapAsync(api);
